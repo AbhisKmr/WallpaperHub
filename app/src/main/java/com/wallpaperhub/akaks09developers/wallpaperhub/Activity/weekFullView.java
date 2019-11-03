5035f025dd753d73c98bbc7836df92d6a24fe9db
naman kumar <sandy@namans-MacBook-Air.local>
2019-11-04 02:48:23 +0530

    fix: data added on all activity's

--- app/src/main/java/com/wallpaperhub/akaks09developers/wallpaperhub/Activity/weekFullView.java
+++ app/src/main/java/com/wallpaperhub/akaks09developers/wallpaperhub/Activity/weekFullView.java
@@ -1,7 +1,10 @@
 package com.wallpaperhub.akaks09developers.wallpaperhub.Activity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -19,11 +22,12 @@ public class weekFullView extends AppCompatActivity {
         setContentView(R.layout.activity_week_full_view);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        String img_url = getIntent().getExtras().getString("image_link");
+        String img_url = getIntent().getExtras().getString("img_url");
         ImageView imageView = findViewById(R.id.weekFullV);
 
 
         RequestOptions requestOptions = new RequestOptions().centerCrop().placeholder(R.drawable.ic_broken_image).error(R.drawable.ic_broken_image);
         Glide.with(this).load(img_url).apply(requestOptions).into(imageView);
     }
+
 }
---
+++ app/src/main/java/com/wallpaperhub/akaks09developers/wallpaperhub/Model/CategoryModel.java
@@ -0,0 +1,21 @@
+package com.wallpaperhub.akaks09developers.wallpaperhub.Model;
+
+public class CategoryModel {
+
+    private String image_link;
+
+    public CategoryModel() {
+    }
+
+    public CategoryModel(String image_link) {
+        this.image_link = image_link;
+    }
+
+    public String getImage_link() {
+        return image_link;
+    }
+
+    public void setImage_link(String image_link) {
+        this.image_link = image_link;
+    }
+}
