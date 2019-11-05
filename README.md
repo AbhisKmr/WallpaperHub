5247f2833cd550c1d00e32a67cd2e20c84c9ba08
AbhisKmr <akaks09@gmail.com>
2019-11-05 11:37:54 +0530

    float button on browse activity

---
+++ README.md
@@ -0,0 +1,6 @@
+echo "# WallpaperHub" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/AbhisKmr/WallpaperHub.git
+git push -u origin master
--- app/src/main/java/com/wallpaperhub/akaks09developers/wallpaperhub/Activity/BrowseFullView.java
+++ app/src/main/java/com/wallpaperhub/akaks09developers/wallpaperhub/Activity/BrowseFullView.java
@@ -1,9 +1,13 @@
 package com.wallpaperhub.akaks09developers.wallpaperhub.Activity;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -11,6 +15,10 @@ import com.bumptech.glide.request.RequestOptions;
 import com.wallpaperhub.akaks09developers.wallpaperhub.R;
 
 public class BrowseFullView extends AppCompatActivity {
+
+    FloatingActionButton fab_more, Fab_download, Fab_Share, FabSetW;
+    Animation FabOpen, FabClose, FabRClockwise, FabRAnticlockwise;
+    boolean isOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,45 @@ public class BrowseFullView extends AppCompatActivity {
 
         RequestOptions requestOptions = new RequestOptions().centerCrop().placeholder(R.drawable.ic_broken_image).error(R.drawable.ic_broken_image);
         Glide.with(this).load(imageUrl).apply(requestOptions).into(bImg);
+
+
+        //get fab button
+        fab_more = findViewById(R.id.fab_more);
+        Fab_download = findViewById(R.id.fab_download);
+        Fab_Share = findViewById(R.id.fab_share);
+        FabSetW = findViewById(R.id.fab_setWallpaper);
+
+        //floating Button Animation
+        FabOpen = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        FabClose = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_colse);
+        FabRClockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_clockwise);
+        FabRAnticlockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_anticlockwise);
+
+        fab_more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isOpen) {
+                    Fab_download.startAnimation(FabClose);
+                    Fab_Share.startAnimation(FabClose);
+                    FabSetW.startAnimation(FabClose);
+                    fab_more.startAnimation(FabRAnticlockwise);
+                    Fab_download.setClickable(false);
+                    Fab_Share.setClickable(false);
+                    FabSetW.setClickable(false);
+                    isOpen = false;
+
+                } else {
+                    Fab_download.startAnimation(FabOpen);
+                    Fab_Share.startAnimation(FabOpen);
+                    FabSetW.startAnimation(FabOpen);
+                    fab_more.startAnimation(FabRClockwise);
+                    Fab_download.setClickable(true);
+                    Fab_Share.setClickable(true);
+                    FabSetW.setClickable(true);
+                    isOpen = true;
+                }
+            }
+        });
 
 
     }
